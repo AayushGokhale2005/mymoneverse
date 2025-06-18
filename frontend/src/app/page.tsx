76a1cbda8d3fbbd5b0face0366163736b5b528ea
Aayush Gokhale <aayushgokhale@Aayushs-MacBook-Air.local>
2025-06-18 11:53:46 -0400

    added and configured tailwind CSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>helloo</h1>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind!
+      </h1>
+    </div>
   );
 }
